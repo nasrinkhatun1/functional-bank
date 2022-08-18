5a084909316f34d4c021ac89248b970c3b135a90
Nasrinkhatun1e <toma32924@gmail.comm>
2022-08-18 21:46:27 +0600

    first commit

---
+++ js/deposit.js
@@ -0,0 +1,18 @@
+
+
+document.getElementById('btn-Deposit').addEventListener('click', function () {
+    const newDepositAmount = getInputFieldValueById('deposit-field');
+    // get previous deposit total by id
+    const previousDepositTotal = getTextElementValueById('deposit-total');
+
+    // calculate new deposit total
+
+    const newDeositTotal = previousDepositTotal + newDepositAmount;
+    // set deposit total value
+    setTextElementValueById('deposit-total', newDeositTotal);
+
+    // get previous balance by using the function 
+    const previousBalanceTotal = getTextElementValueById('balance-total');
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    setTextElementValueById('balance-total', newBalanceTotal);
+})
